0d691ae98f6a501131a89367b509e19810b53c7d
Nguyen Trung Duc <huyga612@gmail.com>
2023-01-30 10:17:35 +0700

    some new build-in function working with arrays

---
+++ Section 11/The new at method/learning.js
@@ -0,0 +1,7 @@
+const arr =[23,11,64]
+console.log(arr[0]) //23
+console.log(arr.at(0))//23
+
+console.log(arr[arr.length - 1]); // get last elemnt manual way
+console.log(arr.at(-1)); // the same
+console.log(arr.slice(-1)[0]);// the same
