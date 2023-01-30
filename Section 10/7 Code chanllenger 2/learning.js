433d491db24833151470e7e9eb4be3214e96307e
Nguyen Trung Duc <huyga612@gmail.com>
2023-01-30 09:25:40 +0700

    Change comment of codechanllenger 2

--- Section 10/7 Code chanllenger 2/learning.js
+++ Section 10/7 Code chanllenger 2/learning.js
@@ -3,3 +3,4 @@
     h1.style.color = 'red'
     document.body.addEventListener('click',() => h1.style.color = 'blue')
 })()
+// when this function run line 2 and 3 gone and function gone but the event still in the body and wait for click
