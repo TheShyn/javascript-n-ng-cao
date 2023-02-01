64a753d02a61c972a234e038d528180586a94da7
Nguyen Trung Duc <huyga612@gmail.com>
2023-02-01 16:29:52 +0700

    UI bankist

---
+++ Section 11/Project bankist app/learning.js
@@ -0,0 +1,24 @@
+const account1 = {
+    owner: 'Jonas',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2,
+    pin: 1111
+}
+const account2 = {
+    owner: 'Jessica',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2,
+    pin: 1111
+}
+const account3 = {
+    owner: 'Johan',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2,
+    pin: 1111
+}
+const account4 = {
+    owner: 'John',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2,
+    pin: 1111
+}
