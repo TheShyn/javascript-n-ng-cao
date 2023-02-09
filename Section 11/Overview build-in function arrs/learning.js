def319897d39f62133b95136d9a78fc4074feaa0
Nguyen Trung Duc <huyga612@gmail.com>
2023-02-09 20:39:08 +0700

    overview about data transformations

---
+++ Section 11/Overview build-in function arrs/learning.js
@@ -0,0 +1,6 @@
+// map 
+    // use map to loop over arrays its similar foreach method but the different is map return an new array
+//filter
+    // use to filter element in the original array which satisfy a certain condition (Thỏa mãn điều kiện nhất định) and return  an new array
+//reduce
+    //use to boils all array element down to one single value
