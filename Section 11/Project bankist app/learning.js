f0dc178bd0ee7e1472c849defdf3c911d58a14b5
Nguyen Trung Duc <huyga612@gmail.com>
2023-02-13 17:55:42 +0700

    Computing usernames

--- Section 11/Project bankist app/learning.js
+++ Section 11/Project bankist app/learning.js
@@ -1,24 +1,53 @@
 const account1 = {
-    owner: 'Jonas',
-    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-    interestRate: 1.2,
-    pin: 1111
-}
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300,2000],
+    interestRate: 1.2, // %
+    pin: 1111,
+};
+
 const account2 = {
-    owner: 'Jessica',
-    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-    interestRate: 1.2,
-    pin: 1111
-}
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
+};
+
 const account3 = {
-    owner: 'Johan',
-    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-    interestRate: 1.2,
-    pin: 1111
-}
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
+};
+
 const account4 = {
-    owner: 'John',
-    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-    interestRate: 1.2,
-    pin: 1111
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
+};
+const histories = document.querySelector('.histories')
+const accounts = [account1, account2, account3, account4];
+const displayMovement = function (movements){
+    movements.forEach((element,i)=>{
+        histories.innerHTML += `
+        <div class="history">
+            <div class="history_infor">
+                <p class="status ${element > 0 ? 'success' : 'err'}">${element > 0 ? 'success' : 'err'}</p>
+                <p class="history_time">${i + 1} Day ago</p>
+            </div>
+            <h4>${element}</h4>
+        </div>
+        `
+    })
 }
+displayMovement(account1.movements)
+const user = 'Steven Thomas Williams'
+const createUserNams = function(accs){
+    accs.forEach(acc=>{
+        acc.username= acc.owner.toLowerCase().split(' ').map(item=>item[0]).join('')
+    })
+}
+createUserNams(accounts)
+console.log(accounts)
+
+
